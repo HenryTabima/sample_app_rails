03d473c5350da7af54fe075b08f1a03109ac29d9
Henry Tabima Giraldo <henrytabima.dev@gmail.com>
2019-05-03 15:43:11 -0500

    Implemented advanced login

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: 'Example User', email: 'user@example.com',
-                     password: "foobar", password_confirmation: "foobar")
+                     password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
@@ -62,13 +62,17 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
+  test 'password should be present (nonblank)' do
+    @user.password = @user.password_confirmation = ' ' * 6
     assert_not @user.valid?
   end
 
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 3
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 3
     assert_not @user.valid?
+  end
+
+  test 'authenticated? should return false for a user with nil digest' do
+    assert_not @user.authenticated?('')
   end
 end
